5c4dc7e3fd7b93d5d66468f009a9499fa539e3cb
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-01-25 21:23:08 -0700

    Update magiclamp.sh

--- magiclamp.sh
+++ magiclamp.sh
@@ -56,8 +56,10 @@ if [[ ${option} == "Custom" ]]; then
     mkdir ${DIR}/HMMs
     mv ${DIR}/*.hmm ${DIR}/HMMs/
     mv ${DIR}/*.HMM ${DIR}/HMMs/
+    echo /home/ark/bin/MagicLamp/MagicLamp.py HmmGenie -bin_dir ${DIR} -bin_ext fa -out ${OUT} -t 4 -hmm_dir ${DIR}/HMMs
     /home/ark/bin/MagicLamp/MagicLamp.py HmmGenie -bin_dir ${DIR} -bin_ext fa -out ${OUT} -t 4 -hmm_dir ${DIR}/HMMs
 else # genie != "custom"
+    echo /home/ark/bin/MagicLamp/MagicLamp.py HmmGenie -bin_dir ${DIR} -bin_ext fa -out ${OUT} -t 4 -hmm_dir ${DIR}/HMMs
     /home/ark/bin/MagicLamp/MagicLamp.py ${option} -bin_dir ${DIR} -bin_ext fa -out ${OUT} -t 4
 fi
 
